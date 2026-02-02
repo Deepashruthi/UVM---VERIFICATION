d0b299f6796b734233b9a08387d323630d812240
DEEPASHRUTHI C <146473846+Deepashruthi@users.noreply.github.com>
2026-02-02 15:47:34 +0530

    Create 04. sequence.sv

---
+++ D Flip Flop/04. sequence.sv
@@ -0,0 +1,25 @@
+// Sequence
+class my_sequence extends uvm_sequence#(my_seq_item);
+  
+  `uvm_object_utils(my_sequence)
+  
+  function new(name="my_sequence");
+    super.new(name);
+  endfunction
+  
+  task body();    
+    my_seq_item req;    
+    `uvm_info("SEQ","Sequence starts",UVM_LOW)    
+    repeat(10) begin   
+    req= my_seq_item::type_id::create("req");
+    start_item(req);
+    if(!req.randomize()) `uvm_error("SEQ", "Randomize failed");
+      finish_item(req);
+    end    
+  endtask
+  
+endclass
+    
+    
+  
+  
