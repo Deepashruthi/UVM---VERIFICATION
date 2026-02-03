6e1d364eb89fe53643e010a03f25d37adc46698d
DEEPASHRUTHI C <146473846+Deepashruthi@users.noreply.github.com>
2026-02-03 15:53:50 +0530

    Create 05. sequencer.sv

---
+++ Counter/05. sequencer.sv
@@ -0,0 +1,11 @@
+//Sequencer
+
+class my_sequencer extends uvm_sequencer #(my_seq_item);
+  
+  `uvm_component_utils(my_sequencer)
+  
+  function new(string name ="Sequencer", uvm_component parent);
+    super.new(name, parent);
+  endfunction
+  
+endclass
