5b5f93b53180411d64711b236e911f1ec85d2caf
DEEPASHRUTHI C <146473846+Deepashruthi@users.noreply.github.com>
2026-02-12 13:59:24 +0530

    Create 08. sequence.sv

---
+++ Asynchronous FIFO/08. sequence.sv
@@ -0,0 +1,20 @@
+//Sequence 
+class fifo_sequence extends uvm_sequence #(fifo_seq_item);
+  `uvm_object_utils(fifo_sequence)
+
+  function new(string name="fifo_sequence");
+    super.new(name);
+  endfunction
+
+  task body();
+    fifo_seq_item req;
+
+    repeat(100) begin
+      req = fifo_seq_item #()::type_id::create("req");
+      start_item(req);
+      if(!req.randomize())
+        `uvm_error("SEQ","Randomization failed");
+      finish_item(req);
+    end
+  endtask
+endclass
